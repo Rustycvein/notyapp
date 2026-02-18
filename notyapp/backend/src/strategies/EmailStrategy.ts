5eaadb9f0fdffb4496a432769b3f77a733c8e6d2
Felix Cordero <felixcordero0428@gmail.com>
2026-02-18 17:16:01 -0400

    fix: cambiar a puerto 587 para evitar timeout en render

--- notyapp/backend/src/strategies/EmailStrategy.ts
+++ notyapp/backend/src/strategies/EmailStrategy.ts
@@ -15,14 +15,15 @@ export class EmailStrategy implements INotificationStrategy {
 
         this.transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
             tls: {
-                rejectUnauthorized: false
+                rejectUnauthorized: false,
+                minVersion: 'TLSv1.2'
             }
         });
     }
