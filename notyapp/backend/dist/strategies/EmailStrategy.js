7bcd62972196c65df3e85b49d10132ef8e9591fd
Felix Cordero <felixcordero0428@gmail.com>
2026-02-20 01:20:26 -0400

    Test

--- notyapp/backend/dist/strategies/EmailStrategy.js
+++ notyapp/backend/dist/strategies/EmailStrategy.js
@@ -4,8 +4,8 @@ export class EmailStrategy {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
