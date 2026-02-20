3c078d9b32ef0ad75d4134c76119eeaff65d7bdb
Felix Cordero <felixcordero0428@gmail.com>
2026-02-20 01:20:07 -0400

    fix: actualizar backend

--- notyapp/backend/src/strategies/EmailStrategy.ts
+++ notyapp/backend/src/strategies/EmailStrategy.ts
@@ -11,8 +11,8 @@ export class EmailStrategy implements INotificationStrategy {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
